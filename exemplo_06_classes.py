c706e92d09d8a6f0fa251210e1ce2925cb17068d
Vitor Prange <12965@proway.treina>
2026-07-15 20:26:59 -0300

    Adiconado exemplo classes

--- exemplo_06_classes.py
+++ exemplo_06_classes.py
@@ -17,14 +17,40 @@ class Cachorro:
         self.idade += 1
         print(f"{self.nome} fez aniversário e agora tem {self.idade} anos.")
 
+def exemplo_cachorro():
+    tobi = Cachorro("Tobi", "Labrador", "Amarelo", 30.0, 5)
+    thor = Cachorro(apelido="Thor", raca="Pastor alemão", cor="Cinza e Branco", peso=25.0, idade=3)
+    mogli = Cachorro("Mogli", "Vira-lata", "Caramelo", 15.0, 2)
 
-tobi = Cachorro("Tobi", "Labrador", "Amarelo", 30.0, 5)
-thor = Cachorro(apelido="Thor", raca="Pastor alemão", cor="Cinza e Branco", peso=25.0, idade=3)
-mogli = Cachorro("Mogli", "Vira-lata", "Caramelo", 15.0, 2)
+    tobi.apresentar_dados()
 
-tobi.apresentar_dados()
+    thor.apresentar_dados()
+    thor.fazer_aniversario()
 
-thor.apresentar_dados()
-thor.fazer_aniversario()
+    mogli.apresentar_dados()
 
-mogli.apresentar_dados()
+
+class Pokemon:
+    #construtor => __init__
+    def __init__(self, nome: str, tipo: str):
+        #Propriedades
+        self.nome = nome
+        self.tipo = tipo
+
+    #funções
+    def apresentar_dados(self):
+        print(f"""Pokemon: {self.nome}
+Tipo: {self.tipo}""")
+
+#orientação a objetos
+def exemplo_pokedex():
+    #Obeto charizard instanciado a partir da classe pokemon
+    charizard = Pokemon("Charizard", "Fogo")
+    bulbasaur = Pokemon("Bulbasaur", "Planta")
+
+    charizard.apresentar_dados()
+    bulbasaur.apresentar_dados()
+
+
+
+exemplo_pokedex()
