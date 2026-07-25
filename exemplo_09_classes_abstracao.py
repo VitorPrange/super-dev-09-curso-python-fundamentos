db0aebea5059bf6c8b60a7b2636c52ca3bc4e113
Vitor Prange <12965@proway.treina>
2026-07-24 21:59:01 -0300

    classes abstração

--- exemplo_09_classes_abstracao.py
+++ exemplo_09_classes_abstracao.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from math import pi
 
 class Animal(ABC):
     def __init__(self, raca: str, idade: int):
@@ -20,4 +21,34 @@ class Cachorro(Animal):
     def fazer_algazarra(self):
         print("Algazarreando")
 
+
+class FormaGeometrica(ABC):
+    @abstractmethod
+    def calcular_area(self) -> float:
+        pass
+
+
+class Circulo(FormaGeometrica):
+    def __init__(self, raio: float):
+        self.raio = raio
+
+    def calcular_area(self) -> float:
+        area = pi * self.raio ** 2
+        return area
     
+class Quadrado(FormaGeometrica):
+    def __init__(self, lado):
+        self.lado = lado
+
+    def calcular_area(self) -> float:
+        return self.lado * self.lado
+        
+
+circulo = Circulo(5)
+
+quadrado = Quadrado(4)
+
+print(f"Area circulo: {circulo.calcular_area()}")
+
+print(f"Area quadrado: {quadrado.calcular_area()}")
+            
